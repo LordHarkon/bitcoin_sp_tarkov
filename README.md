a92300b05be44bbe9789ce65ba9ce4753d4f90dd
Neata Alexandru <frez758@gmail.com>
2024-05-01 20:21:54 +0300

    Initial version

---
+++ README.md
@@ -0,0 +1,3 @@
+# Real Bitcoin Price Mod
+
+This is a mod for SP-Tarkov. It sets the price for the Physical Bitcoin item to the real one from the blockchain.
---
+++ src/mod.ts
@@ -0,0 +1,42 @@
+import { DependencyContainer } from "tsyringe";
+
+import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
+import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+import { IDatabaseTables } from "@spt-aki/models/spt/server/IDatabaseTables";
+import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
+import { HandbookHelper } from "@spt-aki/helpers/HandbookHelper";
+import { PaymentHelper } from "@spt-aki/helpers/PaymentHelper";
+import { LogTextColor } from "@spt-aki/models/spt/logging/LogTextColor";
+
+class Mod implements IPostDBLoadMod {
+    public postDBLoad(container: DependencyContainer): void {
+        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+        const logger = container.resolve<ILogger>("WinstonLogger");
+        const handbookHelper = container.resolve<HandbookHelper>("HandbookHelper");
+        const paymentHelper = container.resolve<PaymentHelper>("PaymentHelper");
+        const bitcoinId = "59faff1d86f7746c51718c9c";
+
+        const tables: IDatabaseTables = databaseServer.getTables();
+
+        const bitcoinHandbook = tables.templates.handbook.Items.find((x) => x.Id === bitcoinId);
+
+        fetch("https://api.blockchain.com/v3/exchange/tickers/BTC-USD")
+            .then((res) => res.json())
+            .then((data) => {
+                const price =
+                    handbookHelper.inRUB(
+                        parseInt(data.price_24h),
+                        paymentHelper.getCurrency("USD")
+                    ) ?? bitcoinHandbook.Price;
+                bitcoinHandbook.Price = price;
+                // transform the price into a string and add a comma to separate thousands
+                const priceString = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                logger.logWithColor(
+                    `[Real Bitcoin Price] Changed bitcoin price to ${priceString} RUB.`,
+                    LogTextColor.GREEN
+                );
+            });
+    }
+}
+
+module.exports = { mod: new Mod() };
